487d6f5694b404f83b6307c132c099797d92619b
GuilhermeBeloni <gbzacarias22@gmail.com>
2022-01-11 17:12:18 -0300

    Incluida função e tela de autenticação.Login/logout

--- agenda/urls.py
+++ agenda/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 from django.views.generic import RedirectView# outro metodo redirect
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
-    path('', RedirectView.as_view(url='/agenda'))#redirect
+    path('', RedirectView.as_view(url='/agenda')),#redirect
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user)
 ]
+
--- core/views.py
+++ core/views.py
@@ -1,12 +1,43 @@
-from django.shortcuts import render #redirect
+from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import  messages
 # Create your views here.
 
 #def index(request):
 #    return redirect('/agenda/')#um dos metodos para redirect
 
+
+
+@login_required(login_url='/login/') # == se não estiver logado não acessa a agenda
 def lista_eventos(request):
+    usuario = request.user
     #usuario = request.user
-    evento = Evento.objects.all()#filter usuario, aparece so os eventos da pessoa logada
+    evento = Evento.objects.filter(usuario=usuario)#filter usuario, aparece só os eventos da pessoa logada//se eu tirar filter e colocar all() mostra todos os eventos
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect(('/'))
+        else:
+            messages.error(request, 'Usuário ou senha inválido')
+    return redirect('/')
+
+
+def login_user(request):
+    return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
